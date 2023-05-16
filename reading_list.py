7da23a012744a5068cbde02df30cb8712afe1324
Eduardo Corona <ecoronalopez@gmail.com>
2023-05-15 17:28:37 -0700

    Tower of Hanoi

---
+++ reading_list.py
@@ -0,0 +1,17 @@
+def reading_list(books):
+    print('Here are the books I will read: ')
+    num_of_books = 0
+    for book in books:
+        print(book)
+        num_of_books += 1
+    print(num_of_books, 'books total.')
+
+def count_book_points(books):
+    points = 0          # 1 step
+    for book in books:  # n * steps in the loop
+        points += 1     # 1 step
+
+    for book in books:                  # n * steps in the loop
+        if 'by Al Sweigart' in book:    # 1 step
+            points += 1                 # 1 step
+    return points                       # 1 step
